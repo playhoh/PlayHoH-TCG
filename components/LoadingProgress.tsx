26aa5789de388b9c2f30d92b8ecae8fd7983ede4
karstenwinter <brox.p@web.de>
2022-05-11 14:28:22 +0200

    fix

--- components/LoadingProgress.tsx
+++ components/LoadingProgress.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import {CircularProgress, Grid, Paper} from "@mui/material"
 
 export function LoadingProgress() {
-    return
     return <Paper style={{padding: 30}}>
         <Grid container justifyContent="center">
             <CircularProgress/>
