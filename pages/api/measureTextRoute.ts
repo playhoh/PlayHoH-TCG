9b054b83fca09bf05914cacf82a9da7fb772dd93
karstenwinter <brox.p@web.de>
2022-08-24 08:20:52 +0200

    fix

--- pages/api/measureTextRoute.ts
+++ pages/api/measureTextRoute.ts
@@ -3,9 +3,9 @@ import {NextApiRequest, NextApiResponse} from "next"
 import {cardTextBoxWidthSVG, cardTextFontSizeSVG} from "../../src/utils"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const id = req.url.substring(req.url.lastIndexOf("/") + 1)
+    const id = decodeURIComponent(req.url.substring(req.url.lastIndexOf("/") + 1))
     res.status(200).json({
-        width: measureText(id),
+        width: measureText(id, cardTextFontSizeSVG),
         splitIntoBoxArray: splitIntoBox(id, cardTextFontSizeSVG, cardTextBoxWidthSVG)
     })
 }
