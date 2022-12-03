3e9d4ddafda1bff60a12d4aa389170ec40fe9f0f
karstenwinter <brox.p@web.de>
2022-12-03 10:45:46 +0100

    WIP moralis replacement: game table and test page

--- pages/db-game-test.tsx
+++ pages/db-game-test.tsx
@@ -43,6 +43,37 @@ const DbTest2Page = () => {
             })
     }
 
+    function loadStateNow() {
+        setDownloading(true)
+        const {a, b} = getPlayers(player1, player2)
+        runCode(`select * from hoh_game where player1="${a}" and player2="${b}"`, () => setDownloading(false), x => {
+            let state = x[0]?.state
+            if (state)
+                setCode(state)
+            setData(x)
+        })
+    }
+
+    function updateStateIfValid(code) {
+        let err = ""
+        try {
+            const js = JSON.parse(code)
+            const {a, b} = getPlayers(player1, player2)
+            setUploading(true)
+            runCode(
+                `
+update hoh_game set timestamp=now(), state='${JSON.stringify(js)}'
+where player1="${escapeSql(a)}" and player2="${escapeSql(b)}"
+`.trim(),
+                () => setUploading(false))
+        } catch (e) {
+            err = e.toString()
+        }
+
+        setErr(err)
+        setCode(code)
+    }
+
     React.useEffect(() => {
         console.log("eff")
         let pending = false
@@ -58,7 +89,12 @@ select timestamp from hoh_game where player1="${escapeSql(a)}" and player2="${es
                     () => pending = false,
                     x => {
                         console.log("interval ", x)
-                        setLastTimestamp(x[0]?.timestamp)
+                        let newTimestamp = x[0]?.timestamp
+                        if (new Date(newTimestamp).getTime() > new Date(lastTimestamp).getTime()) {
+                            if (!uploading)
+                                loadStateNow()
+                        }
+                        setLastTimestamp(newTimestamp)
                     })
             }
         }, 1300)
@@ -86,43 +122,24 @@ select timestamp from hoh_game where player1="${escapeSql(a)}" and player2="${es
                                           inputMode="text"
                                           color="background"
                                           onChange={e => {
-                                              let err = ""
                                               const code = e.target.value
-                                              try {
-                                                  const js = JSON.parse(code)
-                                                  const {a, b} = getPlayers(player1, player2)
-                                                  setUploading(true)
-                                                  runCode(
-                                                      `
-update hoh_game set timestamp=now(), state='${JSON.stringify(js)}'
-where player1="${escapeSql(a)}" and player2="${escapeSql(b)}"
-`.trim(),
-                                                      () => setUploading(false))
-                                              } catch (e) {
-                                                  err = e.toString()
-                                              }
-
-                                              setErr(err)
-                                              setCode(code)
+                                              updateStateIfValid(code)
                                           }} value={code}/>
                     </Grid>
 
                     <Grid item xs={6}>
                         <Button variant="outlined"
                                 onClick={() => {
-                                    const {a, b} = getPlayers(player1, player2)
-                                    runCode(`select * from hoh_game where player1="${a}" and player2="${b}"`)
+                                    loadStateNow()
                                 }}>
                             <PlayCircle fontSize="small"/> &nbsp;{'Load state now'}
                         </Button>
                     </Grid>
 
                     <Grid item xs={6}>
-
                         <Upload fontSize="small" style={{opacity: uploading ? 1 : 0.5}}/> &nbsp;{'Upload'} |
                         <Download fontSize="small" style={{opacity: downloading ? 1 : 0.5}}/> &nbsp;{'Download'} |
                         {lastTimestamp}
-
                     </Grid>
 
                     <Grid item xs={12} marginTop={8}>
